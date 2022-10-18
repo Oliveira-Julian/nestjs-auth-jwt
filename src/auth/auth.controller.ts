af81b7b2f7537504aafdc9c1c28b20fbeb62e99c
Julian Oliveira <oliveira.juliann@gmail.com>
2022-10-17 21:29:54 -0300

    Commit inicial.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+
+import {
+  Controller, HttpCode, HttpStatus, Post, Request, UseGuards
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/auth-request';
+
+
+@Controller('')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @IsPublic()
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  async login(@Request() req: AuthRequest) {
+    return this.authService.login(req.user);
+  }
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  
+  @IsPublic()
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findByEmail() {
+    return this.userService.findByEmail('julian@kaphen.com.br');
+  }
+}
